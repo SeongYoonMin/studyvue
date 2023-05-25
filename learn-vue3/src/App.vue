9d613ebb710ed40ac445a38580c22d218ea728e9
SeongYoonMin <namo4862@naver.com>
2023-05-25 16:18:53 +0900

    Fix

---
+++ learn-vue3/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>{{ refs }}</p>
+		<button type="button" @click="refsHandler(refs, $event)">값 변경</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const refs = ref<number>(300);
+type refsEvents = (args: number, event: Event) => void;
+const refsHandler: refsEvents = (args: number, event: Event) => {
+	refs.value++;
+};
+</script>
+
+<style scoped></style>
