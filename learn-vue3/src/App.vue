1f1fb356891431b698abc271bb56db1b11326405
SeongYoonMin <namo4862@naver.com>
2023-05-30 23:09:06 +0900

    Fix

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,21 +1,67 @@
 <template>
 	<LayoutHeader></LayoutHeader>
-	<div>
-		<PropsStudy
-			:title="titles"
-			:content="true"
-			:new="true"
-			@newProps="titles = '바뀐 첫번째 프롭스'"
-		></PropsStudy>
-	</div>
+
+	<main class="boardList">
+		<div class="list">
+			<EmitStudy
+				v-for="items in boardList"
+				:key="items.id"
+				:title="items.title"
+				:description="items.description"
+				:createAt="items.createAt"
+			/>
+		</div>
+		<button type="button" class="addBoard">새글 추가</button>
+	</main>
 </template>
 
 <script setup lang="ts">
-import PropsStudy from "./components/PropsStudy.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
+import EmitStudy from "./components/EmitStudy.vue";
 import { ref } from "vue";
+interface Board {
+	id: number;
+	title: string;
+	description: string;
+	createAt: string;
+}
 
-const titles = ref<string>('첫번째 프롭스')
+const boardList = ref<Board[]>([
+	{
+		id: 0,
+		title: "제목",
+		description: "이 부분은 설명이 들어가는 부분입니다.",
+		createAt: "2023-05-30",
+	},
+]);
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.boardList {
+	padding: 20px;
+	box-sizing: border-box;
+
+	.list {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		row-gap: 20px;
+		column-gap: 12px;
+	}
+	.addBoard {
+		border: 1px solid #ececec;
+		border-radius: 4px;
+		background-color: white;
+		padding: 4px 8px;
+		color: #616161;
+		font-size: 14px;
+		transition: 0.3s all;
+		cursor: pointer;
+		&:hover {
+			border-color: rgb(255, 193, 181);
+			color: rgb(255, 193, 181);
+		}
+	}
+}
+</style>
---
+++ learn-vue3/src/components/EmitStudy.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="items">
+		<h1 class="title">{{ title }}</h1>
+		<p class="desc">{{ description }}</p>
+		<p class="create">{{ createAt }}</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+	title: string;
+	description: string;
+	createAt: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.items {
+	border: 1px solid #f5f5f5;
+	border-radius: 8px;
+	padding: 12px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: center;
+	line-height: 1.25;
+	color: #333;
+	row-gap: 8px;
+	word-break: keep-all;
+	.title {
+		font-size: 24px;
+		font-weight: 600;
+	}
+
+	.create {
+		font-size: 12px;
+		color: #919191;
+	}
+}
+</style>
